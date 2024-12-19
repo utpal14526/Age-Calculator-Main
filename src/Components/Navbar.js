f3c64bb09cb6ff0b58322136ec6920ed10159fc7
Utpal <itsmeutpalraj@gmail.com>
2024-12-19 14:16:15 +0530

    first message

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Navbar() {
     return (
         <nav className='navbar'>
-            <h1 style={{ color: "white" }}>Calculator<span style={{ color: "green" }}>.net</span></h1>
+            <h1 style={{ color: "white" }}>Calcly<span style={{ color: "green" }}>.net</span></h1>
         </nav>
     )
 }
